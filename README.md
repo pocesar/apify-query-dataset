7c3e2602ae6a40d9a4482a9167e873fd8bc23c11
Paulo Cesar <email@pocesar.e4ward.com>
2020-03-22 15:32:52 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,136 @@
+# Query Dataset
+
+Query your existing datasets, map and generate a subset of your data
+
+Uses MongoDB-like query style, for extended documentation, check [MongoDB query documentation](http://docs.mongodb.org/manual/reference/operator/query/)
+
+It uses [sift](https://www.npmjs.com/package/sift) module for matching, means you can use as query input.
+
+## Example
+
+Take this dataset, for example:
+
+```json
+[
+    {
+        "name": "Name 1",
+        "anotherValue": 1
+    },
+    {
+        "name": "Name 2",
+        "anotherValue": 2
+    },
+    {
+        "name": "",
+        "anotherValue": 3
+    }
+]
+```
+
+You want to query only items that have a name that isn't empty, so you use the following `INPUT`:
+
+```json
+{
+    "datasetId": "YOUR_DATASET_ID",
+    "query": {
+        "name": { "$ne": "" }
+    }
+}
+```
+
+`$ne` means "not equal" in MongoDB, so you'll receive "Name 1" and "Name 2" items.
+
+Now say you want to rename the "name" field to something else:
+
+```json
+{
+    "datasetId": "YOUR_DATASET_ID",
+    "query": {
+        "name": { "$ne": "" }
+    },
+    "filterMap": "item.name = item.name.replace('Name ', ''); item.extra = true; return item;"
+}
+```
+
+Your generated dataset is now:
+
+```json
+[
+    {
+        "name": "1",
+        "anotherValue": 1,
+        "extra": true,
+    },
+    {
+        "name": "2",
+        "anotherValue": 2,
+        "extra": true,
+    }
+]
+```
+
+## filterMap and customOperationSetup
+
+The `filterMap` parameter exists to do even more complex checks. `filterMap` is run in a limited context, and those are the variables available inside your function:
+
+`sift`: the [sift](https://www.npmjs.com/package/sift) module, so you can create a filter on-the-fly
+`console.log`: tied to the 'outside' `console.log` and outputs information to the actor log
+`item`: the current dataset item
+`index`: the current filtered index
+`total`: total items available in the dataset
+`filter`: the created filter from `query` parameter
+`datasetIndex`: the current position in the dataset index
+
+The `customOperationSetup` is mostly useful to prepare a [custom operation](https://www.npmjs.com/package/sift#custom-operations) using `sift`:
+
+```js
+return {
+    $gtDate(params, ownerQuery, options) {
+        const timestamp = new Date(params).getTime();
+
+        return createEqualsOperation(
+            value => new Date(value).getTime() > timestamp, // 'value' here is the date from the field you provide
+            ownerQuery,
+            options
+        );
+    }
+}
+```
+
+then use directly inside your `query` ("2020-01-01" is passed as param to `params`):
+
+```json
+{
+    "query": {
+        "lastModified": { "$gtDate": "2020-01-01" }
+    }
+}
+```
+
+Most of the time, you won't need to use `customOperationSetup`, since the built-in operators can do a lot by themselves, but they are provided for completeness.
+
+## Expected Usage
+
+The memory requirements should be low, but you need at least 256MB, the dataset items aren't loaded all at once in memory, but depending on the shape of your query, you may need more. The more query parameters you provide, more memory and CPU are required, subsequently your query finishes faster.
+
+## Limitations
+
+Some types aren't allowed in JSON, such as `Date` and `RegExp`. The workaround is to define a query without those types, then inside the `filterMap`, you return either null or undefined for dates or RegExp that don't match.
+
+E.g.:
+
+```json
+{
+    "datasetId": "YOUR_DATASET_ID",
+    "query": {
+
+    },
+    "filterMap": "if (new Date(item.someDateField).getTime() < new Date(2019, 10, 20)) { return item }"
+}
+```
+
+Or you can use the `customOperationSetup`
+
+## License
+
+Apache-2.0
---
+++ src/main.js
@@ -0,0 +1,171 @@
+/* eslint-disable no-console */
+const Apify = require('apify');
+const { default: sift, createEqualsOperation, createQueryTester } = require('sift'); // eslint-disable-line no-unused-vars
+const vm = require('vm');
+
+const { log } = Apify.utils;
+
+/**
+ * there's no other way to provide access to the local scope unless we use eval
+ * we want
+ *
+ * @param {string} customOperationSetup
+ */
+const evalSetup = async (customOperationSetup) => {
+    return eval(`(async () => { ${customOperationSetup} })()`); // eslint-disable-line no-eval
+};
+
+Apify.main(async () => {
+    const input = await Apify.getInput();
+    const {
+        datasetId,
+        query,
+        filterMap = 'return item',
+        customOperationSetup = null,
+    } = input;
+
+    if (!datasetId) {
+        throw new Error('You must provide a "datasetId" as string');
+    }
+
+    const datasetInfo = await Apify.client.datasets.getDataset({ datasetId });
+
+    if (!datasetInfo) {
+        throw new Error(`Invalid datasetId provided or you don't have access: ${datasetId}`);
+    }
+
+    const total = datasetInfo.itemCount;
+
+    if (!total) {
+        log.info('Dataset is empty, exiting...');
+
+        return;
+    }
+
+    if (!query || typeof query !== 'object') {
+        throw new Error('You must provide "query" as an object');
+    }
+
+    if (!filterMap || typeof filterMap !== 'string') {
+        throw new Error('Parameter "filterMap" must be a string');
+    }
+
+    if (customOperationSetup && typeof customOperationSetup !== 'string') {
+        throw new Error('Parameter "customOperationSetup" must be a string');
+    }
+
+    let {
+        limit = Infinity,
+        offset = 0,
+    } = input;
+
+    limit = +limit;
+    offset = +offset;
+
+    if (!limit || limit < 0) {
+        throw new Error('The parameter "limit" must be greater than 0');
+    }
+
+    if (Number.isNaN(offset) || offset < 0) {
+        throw new Error('The parameter "offset" must be greater or equal to 0');
+    }
+
+    const c = 'Process took';
+    console.time(c);
+
+    const dataset = await Apify.openDataset(datasetId, { forceCloud: true });
+    /** @type {any} */
+    const available = {
+        sift,
+        total,
+        console: {
+            log: console.log.bind(console),
+        },
+    };
+
+    const parsingContext = (extra = {}) => vm.createContext(Object.assign(Object.create(null), available, extra));
+    /** @type {any} */
+    let operations = {};
+
+    if (customOperationSetup) {
+        let result;
+
+        try {
+            result = await evalSetup(customOperationSetup);
+        } catch (e) {
+            log.exception(e.message, 'customOperationSetup failed');
+            return;
+        }
+
+        if (!result || (typeof result !== 'object' && Array.isArray(result))) {
+            throw new Error('Parameter "customOperationSetup" must return an object');
+        }
+
+        operations = {
+            ...result,
+        };
+    }
+
+    /** @type {Function} */
+    let filter;
+
+    try {
+        filter = sift(query, { operations });
+    } catch (e) {
+        log.exception(e.message, 'sift failed to compile query');
+        return;
+    }
+
+    available.filter = filter;
+    const filterMapFn = vm.compileFunction(filterMap, ['item', 'index', 'datasetIndex'], { parsingContext: parsingContext() });
+
+    let count = 0;
+    let index = -1; // increment when we find something, starts at 0
+
+    log.info(`Starting querying ${datasetInfo.itemCount} dataset items...`);
+
+    try {
+        await dataset.forEach(async (item, datasetIndex) => {
+            if (filter(item)) {
+                index++;
+                // count as "filtered", but skip
+                if (index < offset) {
+                    return;
+                }
+
+                let filtered;
+
+                try {
+                    // a filterMap means that null or undefined get's filtered out
+                    // the result must be either an array or object
+                    filtered = await filterMapFn(item, index, datasetIndex);
+                } catch (e) {
+                    log.exception(e.message, 'filterMap failed', { index, datasetIndex });
+                    return;
+                }
+
+                if (filtered !== null && filtered !== undefined) {
+                    if (typeof filtered === 'object') {
+                        count++;
+                        await Apify.pushData(filtered);
+
+                        if (count >= limit) {
+                            throw new Error('break');
+                        }
+                    } else {
+                        log.warning('Return value of filterMap is not an object or array', { index, datasetIndex, filtered });
+                    }
+                }
+            }
+        });
+    } catch (e) {
+        // we expect a break in case of "limit", the only way to
+        // stop dataset.forEach
+        if (e.message !== 'break') {
+            log.exception(e);
+        }
+    }
+
+    console.timeEnd(c);
+    log.info(`Generated ${count} items using ${(process.memoryUsage().heapTotal / 1024 / 1024).toFixed(2)}MB`);
+});
